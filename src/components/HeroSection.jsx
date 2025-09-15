f4f90fc9fd6cba1fb2568097e2a2aa8c5ebfbb8a
Mohit Mudgil <mohittt0011@gmail.com>
2025-09-15 09:12:27 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -27,6 +27,7 @@ const HeroSection = () => {
           autoPlay
           loop
           muted
+          infinite
           className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4"
         >
           <source src={video1} type="video/mp4" />
@@ -36,6 +37,7 @@ const HeroSection = () => {
           autoPlay
           loop
           muted
+          infinite
           className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4"
         >
           <source src={video2} type="video/mp4" />
